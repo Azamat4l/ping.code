2734cb93f08813ec5e3227dc47e70c6263a70be8
Azamat4lk_774 <129657456+Azamat4l@users.noreply.github.com>
2023-06-05 16:19:02 +0500

    Add files via upload

--- pin_pong.py
+++ pin_pong.py
@@ -26,10 +26,10 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y<window_height-80:
             self.rect.y+=self.speed
 
-back=(255,255,255)
-window_width=500
+back=(66,170,255)
+window_width=600 
 window_height=500
-window=display.set_mode(window_width, window_height)
+window=display.set_mode((window_width, window_height))
 window.fill(back)
 
 game=True
@@ -38,11 +38,43 @@ clock=time.Clock()
 
 FPS=60
 
+racket1=Player("pixil-frame-0.png",30,200,50,150,4)
+racket2=Player("pixil-frame-0.png",520,200,50,150,4)
+ball=GameSprite("tenis_ball.png",200,200,50,50,4)
+
+font.init()
+font=font.SysFont(None,35)
+lose1=font.render('Player 1 lose',True,(180,0,0))
+lose2=font.render('Player 2 lose',True,(180,0,0))
+
+speed_x=3
+speed_y=3
+
 while game:
-    for r in event.get:
+    for e in event.get():
         if e.type == QUIT:
             game=False
     if finish!=True:
         window.fill(back)
+        racket1.update_r()
+        racket2.update_l()
+        ball.rect.x+=speed_x
+        ball.rect.y+=speed_y
+        if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2,ball):
+            speed_x*=-1
+            speed_y*=-1
+        if ball.rect.y > window_height-50 or ball.rect.y<0:
+            speed_y*=-1
+        if ball.rect.x <0:
+            finish=True
+            window.blit(lose1,(200,200))
+            game_over=True
+        if ball.rect.x >window_width:
+            finish=True
+            window.blit(lose2,(200,200))
+            game_over=True
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
     display.update()
     clock.tick(FPS)
